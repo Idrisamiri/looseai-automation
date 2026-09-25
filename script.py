dc1c131ba5c364cca62a7aa1fc5ba5cf5ba2f5e4
Idrisamiri <idris2.amiry@gmail.com>
2026-09-25 17:31:04 +0300

    Update script.py

--- script.py
+++ script.py
@@ -39,7 +39,7 @@ def generate_concept():
 
 
 def generate_image(prompt):
-    url = "https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-xl-base-1.0"
+    url = "https://router.huggingface.co/hf-inference/models/stabilityai/stable-diffusion-xl-base-1.0"
     headers = {"Authorization": f"Bearer {HF_API_KEY}"}
 
     resp = None
